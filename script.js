30bff594bd675d62114de871e26aba8c3416ee2a
Winstone Anderson <93783070+winstone01@users.noreply.github.com>
2026-08-22 11:47:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -15,12 +15,16 @@ function calculate() {
 
     if (bmi < 18.5) {
         category = 'Underweight'
+        result.style.color = '#ffc44d'
     } else if (bmi >= 18.5 && bmi <= 24.9) {
         category = 'Normal Weight';
+        result.style.color = "#0be881";
     } else if (bmi >= 25 && bmi <= 29.9) {
         category = 'Over Weight';
+        result.style.color = "#ff884d";
     } else {
         category = 'Obese';
+        result.style.color = "#ff5e57";
     }
     document.getElementById("category").textContent = category;
 }
